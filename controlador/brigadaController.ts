840f88a8d7289412324bbdce08929b7872bf1dd3
Santiago Ortiz <santiago.ortizb06@gmail.com>
2025-04-15 10:57:10 -0500

    Correcciones CRUD brigada y pruebas de POSTMAN.

--- controlador/brigadaController.ts
+++ controlador/brigadaController.ts
@@ -2,6 +2,11 @@
 import { Request, Response } from "express";
 import * as brigadaService from "../servicios/brigadaService.ts";
 
+/**
+ * Funcion asincronica que llama al controlador de obtener Brigadas y las trae en una lista
+ * @param req 
+ * @param res 
+ */
 export const obtenerBrigadasController = async (req:Request, res: Response) => {
     try {
         const brigada = await brigadaService.obtenerBrigadas();
@@ -11,15 +16,26 @@ export const obtenerBrigadasController = async (req:Request, res: Response) => {
     }
 }
 
+/**
+ * Funcion para crear una brigada con req.body lo crea exactamente como esta mapeado
+ * @param req 
+ * @param res 
+ */
 export const crearBrigadasController = async (req:Request, res: Response) => {
     try {
         const brigadaCreada = await brigadaService.crearBrigada(req.body);
         res.status(201).json(brigadaCreada);
-    } catch (error) {
-        res.status(500);
+    } catch (error: any) {
+        console.error("Error al crear brigada:", error);
+        res.status(500).json({ error: error.message || "Error del servidor" });
     }
 }
 
+/**
+ * Funcion para crear una brigada, se pasa un id y los datos a cambiar.
+ * @param req 
+ * @param res 
+ */
 export const actualizarBrigadasController = async (req:Request, res: Response) => {
     const { id } = req.params;
     try {
@@ -33,6 +49,11 @@ export const actualizarBrigadasController = async (req:Request, res: Response) =
     }
 }
 
+/**
+ * 
+ * @param req 
+ * @param res 
+ */
 export const eliminarBrigadasController = async (req:Request, res: Response) => {
     const { id } = req.params;
     try {
@@ -43,6 +64,11 @@ export const eliminarBrigadasController = async (req:Request, res: Response) => 
     }
 }
 
+/**
+ * 
+ * @param req 
+ * @param res 
+ */
 export const obtenerBrigadaPorIdController  = async (req:Request, res: Response) => {
     const { id } = req.params;
     try {
